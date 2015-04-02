7a2062f9b524618d7859aabe548a0eafd79844c8
Tim Dysinger <tim@dysinger.net>
2015-04-02 09:46:19 -1000

    initial impl
    ```
    tim@humuhumunukunukuapuaa:~/src/github.com/dysinger/bouncy% curl -vv
    http://localhost:4321/test\?cig\=red
    *   Trying 127.0.0.1...
    * Connected to localhost (127.0.0.1) port 4321 (#0)
    > GET /test?cig=red HTTP/1.1
    > User-Agent: curl/7.40.0
    > Host: localhost:4321
    > Accept: */*
    >
    < HTTP/1.1 301 Moved Permanently
    < Transfer-Encoding: chunked
    < Date: Thu, 02 Apr 2015 19:44:42 GMT
    < Server: Warp/3.0.10.1
    < Location: https://localhost/test?cig=red
    <
    * Connection #0 to host localhost left intact
    ```

--- Main.hs
+++ Main.hs
@@ -1,1 +1,29 @@
-main = return ()
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+import BasePrelude
+import qualified Data.ByteString.Char8 as C ( split )
+import Network.Wai
+    ( Request(rawPathInfo, rawQueryString, requestHeaderHost),
+      responseBuilder )
+import Network.Wai.Handler.Warp ( run )
+import Network.HTTP.Types ( status406, status301 )
+
+main :: IO ()
+main = run 4321 bounce
+  where bounce rq rs
+          | isJust (requestHeaderHost rq) =
+            let host =
+                  case C.split ':' (fromJust (requestHeaderHost rq)) of
+                    (h:[]) -> h
+                    (h:_) -> h
+                https =
+                  "https://" <> host <>
+                  (rawPathInfo rq) <>
+                  (rawQueryString rq)
+            in rs (responseBuilder status301
+                                   [("Location",https)]
+                                   mempty)
+        bounce _ rs
+          | otherwise =
+            rs (responseBuilder status406 [] "This service requires a Host header")
