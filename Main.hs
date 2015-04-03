93348ac03fd5a5b589cbbe5b714e5aa47c88c7eb
Tim Dysinger <tim@dysinger.net>
2015-04-02 18:47:35 -1000

    Add the following
    optparse package (command line options)
    added -p --port and -h --help options
    added status 307 on non-GET requests

--- Main.hs
+++ Main.hs
@@ -1,16 +1,68 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
 
-import BasePrelude
+{-|
+Module      : Main
+Description : Simply redirects HTTP->HTTPS (e.g. use with AWS ELB)
+Copyright   : (c) FPComplete.com, 2015
+License     : BSD3
+Maintainer  : Tim Dysinger <tim@fpcomplete.com>
+Stability   : experimental
+Portability : POSIX
+-}
+
+module Main where
+
+import           BasePrelude
 import qualified Data.ByteString.Char8 as C ( split )
+import Data.Time ( getCurrentTime )
+import Distribution.PackageDescription.TH
+    ( PackageDescription(package),
+      PackageIdentifier(pkgVersion),
+      packageVariable )
+import Language.Haskell.TH ( runIO, stringE )
+import Network.HTTP.Types
+    ( status406, status307, status301, methodGet )
 import Network.Wai
-    ( Request(rawPathInfo, rawQueryString, requestHeaderHost),
+    ( Request(rawPathInfo, rawQueryString, requestHeaderHost,
+              requestMethod),
       responseBuilder )
 import Network.Wai.Handler.Warp ( run )
-import Network.HTTP.Types ( status406, status301 )
+import Options.Applicative
+    ( helper,
+      execParser,
+      value,
+      short,
+      progDesc,
+      option,
+      metavar,
+      long,
+      info,
+      help,
+      header,
+      fullDesc,
+      auto )
 
+-- | Main entry point.
 main :: IO ()
-main = run 4321 bounce
+main =
+  join (execParser
+          (info (helper <*>
+                 (flip run bounce <$>
+                  option auto
+                         (short 'p' <>
+                          long "port" <>
+                          metavar "PORT" <>
+                          value 4321 <>
+                          help "Port for the webserver")))
+                (fullDesc <>
+                 header ("Bouncy " <>
+                         $(packageVariable (pkgVersion . package)) <>
+                         " " <>
+                         $(stringE =<<
+                           runIO (show `fmap` Data.Time.getCurrentTime))) <>
+                 progDesc "Bounces HTTP->HTTPS (e.g., AWS ELB)")))
   where bounce rq rs
           | isJust (requestHeaderHost rq) =
             let (host:_) =
@@ -19,7 +71,11 @@ main = run 4321 bounce
                   "https://" <> host <>
                   (rawPathInfo rq) <>
                   (rawQueryString rq)
-            in rs (responseBuilder status301
+                status =
+                  if requestMethod rq == methodGet
+                     then status301
+                     else status307
+            in rs (responseBuilder status
                                    [("Location",https)]
                                    mempty)
         bounce _ rs
